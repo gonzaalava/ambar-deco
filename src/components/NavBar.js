8e7b4dc057c65f57af46423f90cd036b04d02149
Gonzalo Alava <gonzaalava@gmail.com>
2021-03-15 10:32:21 -0300

    Cambios Navbar
    Navbar add

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+  return (
+    <div className="App">
+      <ul>
+  <li><a href="default.asp">INICIO</a></li>
+  <li><a href="news.asp">NOVEDADES</a></li>
+  <li><a href="contact.asp">CONTACTO</a></li>
+  <li><a href="about.asp">SOBRE NOSOTROS</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
